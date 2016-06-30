392a6caff62885506f279be9b2285b3e08e4d6ff
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2016-06-30 07:05:57 +0000

    fix(redis/entrypoint.sh) move run-specific configuration into run.sh

--- redis/entrypoint.sh
+++ redis/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -e
 
-IPADDR=$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
-
-if [ "$1" = 'redis-server' ]; then
+if [ "$1" = '/run.sh' ]; then
   chown -R redis .
-  sed "s/^bind .*$/bind ${IPADDR}/" /etc/redis/redis.conf > /tmp/redis.conf
-  exec su-exec redis "$1" /tmp/redis.conf
+  exec su-exec redis "$@"
 fi
 
 exec "$@"
