1ee7fa60b86b096b7dae309b94c9c690d2c9163c
Ruiwen Chua <ruiwen@lomotif.com>
2017-09-19 08:27:02 +0000

    fix(redis/run.sh) use redis.conf instead of master.conf

--- redis/run.sh
+++ redis/run.sh
@@ -97,9 +97,9 @@ launchmaster() {
 
   IPADDR=$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
   IPADDR="${IPADDR} 127.0.0.1"
-  sed -i "s/^bind .*$/bind ${IPADDR}/" /etc/redis/master.conf
-  sed -i "s/%maxmemory%/${MAXMEMORY}/" /etc/redis/master.conf
-  redis-server /etc/redis/master.conf
+  sed -i "s/^bind .*$/bind ${IPADDR}/" /etc/redis/redis.conf
+  sed -i "s/%maxmemory%/${MAXMEMORY}/" /etc/redis/redis.conf
+  redis-server /etc/redis/redis.conf
 }
 
 launchsentinel() {
